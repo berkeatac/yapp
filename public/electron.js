b12cbc075e4b95622f3c623cca93af287c1cf310
berkeatac <berkeatac@sabanciuniv.edu>
2020-10-05 01:05:30 +0300

    App converted to tray

--- public/electron.js
+++ public/electron.js
@@ -7,22 +7,104 @@ const isDev = require("electron-is-dev");
 
 let mainWindow;
 
-require("update-electron-app")({
-  repo: "kitze/react-electron-example",
-  updateInterval: "1 hour"
-});
+// require("update-electron-app")({
+//   repo: "kitze/react-electron-example",
+//   updateInterval: "1 hour",
+// });
 
-function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680, webPreferences: { nodeIntegration: true }});
+const createTray = () => {
+  tray = new electron.Tray(path.join(__dirname, "icon.png"));
+  tray.on("right-click", toggleWindow);
+  tray.on("double-click", toggleWindow);
+  tray.on("click", function(event) {
+    toggleWindow();
+
+    // Show devtools when command clicked
+    if (mainWindow.isVisible() && process.defaultApp && event.metaKey) {
+      mainWindow.openDevTools({ mode: "detach" });
+    }
+  });
+};
+
+// function createWindow() {
+//   mainWindow = new BrowserWindow({
+//     width: 900,
+//     height: 680,
+//     webPreferences: { nodeIntegration: true },
+//   });
+//   mainWindow.loadURL(
+//     isDev
+//       ? "http://localhost:3000"
+//       : `file://${path.join(__dirname, "../build/index.html")}`
+//   );
+//   mainWindow.on("closed", () => (mainWindow = null));
+// }
+
+const createWindow = () => {
+  mainWindow = new electron.BrowserWindow({
+    width: 500,
+    height: 450,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+    webPreferences: {
+      // Prevents renderer process code from not running when window is
+      // hidden
+      backgroundThrottling: false,
+      nodeIntegration: true,
+    },
+  });
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
       : `file://${path.join(__dirname, "../build/index.html")}`
   );
-  mainWindow.on("closed", () => (mainWindow = null));
-}
 
-app.on("ready", createWindow);
+  // Hide the window when it loses focus
+  mainWindow.on("blur", () => {
+    if (!mainWindow.webContents.isDevToolsOpened()) {
+      mainWindow.hide();
+    }
+  });
+};
+
+const iconPath = path.join(__dirname, "icon.png");
+app.on("ready", () => {
+  createWindow();
+  createTray();
+});
+
+const getWindowPosition = () => {
+  const windowBounds = mainWindow.getBounds();
+  const trayBounds = tray.getBounds();
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(
+    trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2
+  );
+
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 4);
+
+  return { x: x, y: y };
+};
+
+const toggleWindow = () => {
+  if (mainWindow.isVisible()) {
+    mainWindow.hide();
+  } else {
+    showWindow();
+  }
+};
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  mainWindow.setPosition(position.x, position.y, false);
+  mainWindow.show();
+  mainWindow.focus();
+};
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const {app} = window.require('electron').remote;
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-class App extends Component {
-  render() {
-    return (
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const { app } = window.require("electron").remote;
+
+const App = () => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
+          <h2>
+            React + Electron ={" "}
+            <span role="img" aria-label="love">
+              😍
+            </span>
+          </h2>
         </div>
         <p className="App-intro">
           <b> Release 0.2.7 </b>
           Version: {app.getVersion()}
         </p>
       </div>
-    );
-  }
-}
+    </>
+  );
+};
 
 export default App;
